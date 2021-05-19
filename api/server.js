e3d9277bd4e379d14beff9d7d5ba75aeee732180
sjors37 <sjorshekerman@LIVE.NL>
2021-05-19 14:01:23 +0200

    error fixes

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config();
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
 }
 
 const express = require('express');
